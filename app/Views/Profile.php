60cfb237265c76f2ebf53a1c4826f688184fbed8
unknown <iarisaldy@gmail.com>
2020-06-25 00:21:47 +0700

    menambahkan template profile

--- app/Views/Profile.php
+++ app/Views/Profile.php
@@ -3,34 +3,28 @@
 
 <head>
 	<meta charset="utf-8">
-	<title>Regna Bootstrap Template</title>
+	<title>Belajar</title>
 	<meta content="width=device-width, initial-scale=1.0" name="viewport">
 	<meta content="" name="keywords">
 	<meta content="" name="description">
 
 	<!-- Favicons -->
-	<link href="<?php echo base_url() . 'assets/img/favicon.png' ?>" rel="icon">
-	<link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+	<link href="<?php echo base_url('/CI4-Web-Profile/img/favicon.png'); ?>" rel="icon">
+	<link href="<?php echo base_url('/CI4-Web-Profile/img/apple-touch-icon.png'); ?>" rel="apple-touch-icon">
 
 	<!-- Google Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700" rel="stylesheet">
 
 	<!-- Bootstrap CSS File -->
-	<link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?php echo base_url('/CI4-Web-Profile/assets/lib/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
 
 	<!-- Libraries CSS Files -->
-	<link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-	<link href="lib/animate/animate.min.css" rel="stylesheet">
+	<link href="<?php echo base_url('/CI4-Web-Profile/assets/lib/font-awesome/css/font-awesome.min.css'); ?>" rel="stylesheet">
+	<link href="<?php echo base_url('/CI4-Web-Profile/assets/lib/animate/animate.min.css'); ?>" rel="stylesheet">
 
 	<!-- Main Stylesheet File -->
-	<link href="css/style.css" rel="stylesheet">
+	<link href="<?php echo base_url('/CI4-Web-Profile/assets/css/style.css'); ?>" rel="stylesheet">
 
-	<!-- =======================================================
-    Theme Name: Regna
-    Theme URL: https://bootstrapmade.com/regna-bootstrap-onepage-template/
-    Author: BootstrapMade.com
-    License: https://bootstrapmade.com/license/
-  ======================================================= -->
 </head>
 
 <body>
@@ -42,7 +36,7 @@
 		<div class="container">
 
 			<div id="logo" class="pull-left">
-				<a href="#hero"><img src="img/logo.png" alt="" title="" /></img></a>
+				<a href="#hero"><img src="#" alt="" title="" /></img></a>
 				<!-- Uncomment below if you prefer to use a text logo -->
 				<!--<h1><a href="#hero">Regna</a></h1>-->
 			</div>
@@ -82,9 +76,9 @@
   ============================-->
 	<section id="hero">
 		<div class="hero-container">
-			<h1>Welcome to Regna</h1>
-			<h2>We are team of talanted designers making websites with Bootstrap</h2>
-			<a href="#about" class="btn-get-started">Get Started</a>
+			<h1>Welcome to Belajar</h1>
+			<h2>Kami adalah kumpulan orang yang sedang belajar ci 4</h2>
+			<a href="#about" class="btn-get-started">Yuk jangan males</a>
 		</div>
 	</section><!-- #hero -->
 
@@ -266,7 +260,7 @@
 				<div class="row" id="portfolio-wrapper">
 					<div class="col-lg-3 col-md-6 portfolio-item filter-app">
 						<a href="">
-							<img src="img/portfolio/app1.jpg" alt="">
+							<img src="<?php echo base_url('/CI4-Web-Profile/img/portfolio/app1.jpg'); ?>" alt="">
 							<div class="details">
 								<h4>App 1</h4>
 								<span>Alored dono par</span>
@@ -276,7 +270,7 @@
 
 					<div class="col-lg-3 col-md-6 portfolio-item filter-web">
 						<a href="">
-							<img src="img/portfolio/web2.jpg" alt="">
+							<img src="<?php echo base_url('/CI4-Web-Profile/img/portfolio/web2.jpg'); ?>" alt="">
 							<div class="details">
 								<h4>Web 2</h4>
 								<span>Alored dono par</span>
@@ -286,7 +280,7 @@
 
 					<div class="col-lg-3 col-md-6 portfolio-item filter-app">
 						<a href="">
-							<img src="img/portfolio/app3.jpg" alt="">
+							<img src="<?php echo base_url('/CI4-Web-Profile/img/portfolio/app3.jpg'); ?>" alt="">
 							<div class="details">
 								<h4>App 3</h4>
 								<span>Alored dono par</span>
@@ -296,7 +290,7 @@
 
 					<div class="col-lg-3 col-md-6 portfolio-item filter-card">
 						<a href="">
-							<img src="img/portfolio/card1.jpg" alt="">
+							<img src="<?php echo base_url('/CI4-Web-Profile/img/portfolio/card1.jpg'); ?>" alt="">
 							<div class="details">
 								<h4>Card 1</h4>
 								<span>Alored dono par</span>
@@ -306,7 +300,7 @@
 
 					<div class="col-lg-3 col-md-6 portfolio-item filter-card">
 						<a href="">
-							<img src="img/portfolio/card2.jpg" alt="">
+							<img src="<?php echo base_url('/CI4-Web-Profile/img/portfolio/card2.jpg'); ?>" alt="">
 							<div class="details">
 								<h4>Card 2</h4>
 								<span>Alored dono par</span>
@@ -316,7 +310,7 @@
 
 					<div class="col-lg-3 col-md-6 portfolio-item filter-web">
 						<a href="">
-							<img src="img/portfolio/web3.jpg" alt="">
+							<img src="<?php echo base_url('/CI4-Web-Profile/img/portfolio/web3.jpg'); ?>" alt="">
 							<div class="details">
 								<h4>Web 3</h4>
 								<span>Alored dono par</span>
@@ -326,7 +320,7 @@
 
 					<div class="col-lg-3 col-md-6 portfolio-item filter-card">
 						<a href="">
-							<img src="img/portfolio/card3.jpg" alt="">
+							<img src="<?php echo base_url('/CI4-Web-Profile/img/portfolio/card3.jpg'); ?>" alt="">
 							<div class="details">
 								<h4>Card 3</h4>
 								<span>Alored dono par</span>
@@ -336,7 +330,7 @@
 
 					<div class="col-lg-3 col-md-6 portfolio-item filter-app">
 						<a href="">
-							<img src="img/portfolio/app2.jpg" alt="">
+							<img src="<?php echo base_url('/CI4-Web-Profile/img/portfolio/app2.jpg'); ?>" alt="">
 							<div class="details">
 								<h4>App 2</h4>
 								<span>Alored dono par</span>
@@ -346,7 +340,7 @@
 
 					<div class="col-lg-3 col-md-6 portfolio-item filter-logo">
 						<a href="">
-							<img src="img/portfolio/logo1.jpg" alt="">
+							<img src="<?php echo base_url('/CI4-Web-Profile/img/portfolio/logo1.jpg'); ?>" alt="">
 							<div class="details">
 								<h4>Logo 1</h4>
 								<span>Alored dono par</span>
@@ -356,7 +350,7 @@
 
 					<div class="col-lg-3 col-md-6 portfolio-item filter-logo">
 						<a href="">
-							<img src="img/portfolio/logo3.jpg" alt="">
+							<img src="<?php echo base_url('/CI4-Web-Profile/img/portfolio/logo3.jpg'); ?>" alt="">
 							<div class="details">
 								<h4>Logo 3</h4>
 								<span>Alored dono par</span>
@@ -366,7 +360,7 @@
 
 					<div class="col-lg-3 col-md-6 portfolio-item filter-web">
 						<a href="">
-							<img src="img/portfolio/web1.jpg" alt="">
+							<img src="<?php echo base_url('/CI4-Web-Profile/img/portfolio/web1.jpg'); ?>" alt="">
 							<div class="details">
 								<h4>Web 1</h4>
 								<span>Alored dono par</span>
@@ -376,7 +370,7 @@
 
 					<div class="col-lg-3 col-md-6 portfolio-item filter-logo">
 						<a href="">
-							<img src="img/portfolio/logo2.jpg" alt="">
+							<img src="<?php echo base_url('/CI4-Web-Profile/img/portfolio/logo2.jpg'); ?>" alt="">
 							<div class="details">
 								<h4>Logo 2</h4>
 								<span>Alored dono par</span>
@@ -401,9 +395,9 @@
 				<div class="row">
 					<div class="col-lg-3 col-md-6">
 						<div class="member">
-							<div class="pic"><img src="img/team-1.jpg" alt=""></div>
-							<h4>Walter White</h4>
-							<span>Chief Executive Officer</span>
+							<div class="pic"><img src="<?php echo base_url('/CI4-Web-Profile/img/team-1.jpg'); ?>" alt=""></div>
+							<h4>Irfan Arisaldy</h4>
+							<span>Developer</span>
 							<div class="social">
 								<a href=""><i class="fa fa-twitter"></i></a>
 								<a href=""><i class="fa fa-facebook"></i></a>
@@ -415,9 +409,9 @@
 
 					<div class="col-lg-3 col-md-6">
 						<div class="member">
-							<div class="pic"><img src="img/team-2.jpg" alt=""></div>
-							<h4>Sarah Jhinson</h4>
-							<span>Product Manager</span>
+							<div class="pic"><img src="<?php echo base_url('/CI4-Web-Profile/img/team-2.jpg'); ?>" alt=""></div>
+							<h4>Agil Abdullah</h4>
+							<span>Developer</span>
 							<div class="social">
 								<a href=""><i class="fa fa-twitter"></i></a>
 								<a href=""><i class="fa fa-facebook"></i></a>
@@ -429,9 +423,9 @@
 
 					<div class="col-lg-3 col-md-6">
 						<div class="member">
-							<div class="pic"><img src="img/team-3.jpg" alt=""></div>
-							<h4>William Anderson</h4>
-							<span>CTO</span>
+							<div class="pic"><img src="<?php echo base_url('/CI4-Web-Profile/img/team-3.jpg'); ?>" alt=""></div>
+							<h4>Ramadhan</h4>
+							<span>Developer</span>
 							<div class="social">
 								<a href=""><i class="fa fa-twitter"></i></a>
 								<a href=""><i class="fa fa-facebook"></i></a>
@@ -443,9 +437,9 @@
 
 					<div class="col-lg-3 col-md-6">
 						<div class="member">
-							<div class="pic"><img src="img/team-4.jpg" alt=""></div>
-							<h4>Amanda Jepson</h4>
-							<span>Accountant</span>
+							<div class="pic"><img src="<?php echo base_url('/CI4-Web-Profile/img/team-4.jpg'); ?>" alt=""></div>
+							<h4>Rozako</h4>
+							<span>Developer</span>
 							<div class="social">
 								<a href=""><i class="fa fa-twitter"></i></a>
 								<a href=""><i class="fa fa-facebook"></i></a>
@@ -567,21 +561,20 @@
 	<a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
 
 	<!-- JavaScript Libraries -->
-	<script src="lib/jquery/jquery.min.js"></script>
-	<script src="lib/jquery/jquery-migrate.min.js"></script>
-	<script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
-	<script src="lib/easing/easing.min.js"></script>
-	<script src="lib/wow/wow.min.js"></script>
-	<script src="lib/waypoints/waypoints.min.js"></script>
-	<script src="lib/counterup/counterup.min.js"></script>
-	<script src="lib/superfish/hoverIntent.js"></script>
-	<script src="lib/superfish/superfish.min.js"></script>
+	<script src="<?php echo base_url('/CI4-Web-Profile/assets/lib/jquery/jquery.min.js'); ?>"></script>
+	<script src="<?php echo base_url('/CI4-Web-Profile/assets/lib/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
+	<script src="<?php echo base_url('/CI4-Web-Profile/assets/lib/easing/easing.min.js'); ?>"></script>
+	<script src="<?php echo base_url('/CI4-Web-Profile/assets/lib/wow/wow.min.js'); ?>"></script>
+	<script src="<?php echo base_url('/CI4-Web-Profile/assets/lib/waypoints/waypoints.min.js'); ?>"></script>
+	<script src="<?php echo base_url('/CI4-Web-Profile/assets/lib/counterup/counterup.min.js'); ?>"></script>
+	<script src="<?php echo base_url('/CI4-Web-Profile/assets/lib/superfish/hoverIntent.js'); ?>"></script>
+	<script src="<?php echo base_url('/CI4-Web-Profile/assets/lib/superfish/superfish.min.js'); ?>"></script>
 
 	<!-- Contact Form JavaScript File -->
-	<script src="contactform/contactform.js"></script>
-
+	<script src="<?php echo base_url('/CI4-Web-Profile/assets/contactform/contactform.js'); ?>"></script>
+	1
 	<!-- Template Main Javascript File -->
-	<script src="js/main.js"></script>
+	<script src="<?php echo base_url('/CI4-Web-Profile/assets/js/main.js'); ?>"></script>
 
 </body>
 
