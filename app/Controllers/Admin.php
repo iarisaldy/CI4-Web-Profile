64ea326d167cdcb24888f8331f41712c1257a6c6
unknown <iarisaldy@gmail.com>
2020-07-03 22:03:16 +0700

    penambahan fungsi login

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -4,15 +4,25 @@ namespace App\Controllers;
 
 class Admin extends BaseController
 {
-	public function index()
+	public function __construct()
 	{
-		return view('Administrator.php');
+		helper('form');
 	}
 
 	public function login()
-    {
-        return view('Login.php');
-    }
-	//--------------------------------------------------------------------
+	{
+		return view('Login.php');
+	}
 
+	public function get_user()
+	{
+		$email   = $this->request->getPost('email');
+		$password  = $this->request->getPost('password');
+
+		if ($email == 'admin' && $password == 'admin') {
+			return view('Administrator.php');
+		} else {
+			echo "error gk bisa login";
+		}
+	}
 }
