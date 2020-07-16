4337e1d1f41f8c9200a38d5d56ae71eb0923379e
unknown <iarisaldy@gmail.com>
2020-07-16 09:05:14 +0700

    menambahkan model

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -34,7 +34,7 @@ class Database extends \CodeIgniter\Database\Config
 		'hostname' => 'localhost',
 		'username' => '',
 		'password' => '',
-		'database' => '',
+		'database' => 'belajar',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
