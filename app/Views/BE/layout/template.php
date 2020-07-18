7c54d9fd5948d5996ff2f920d8f3364c77647e6c
Ramadhan Dadan <ramadhanrsa18@gmail.com>
2020-07-19 01:20:11 +0700

    Tambah tabel

--- app/Views/BE/layout/template.php
+++ app/Views/BE/layout/template.php
@@ -386,6 +386,91 @@
 		</div>
 	</div>
 
+	<!-- Tambah User Modal-->
+	<div class="modal fade" id="TambahModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel">Tambah User?</h5>
+					<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">×</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<label for="basic-url">Judul</label>
+					<div class="input-group mb-3">
+						<input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+					</div>
+
+					<label for="basic-url">Upload Foto</label>
+					<div class="input-group mb-3">
+						<div class="custom-file">
+							<input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
+							<label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+						</div>
+					</div>
+
+				</div>
+				<div class="modal-footer">
+					<button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+					<a class="btn btn-primary" href="Index">Save</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Edit Modal-->
+	<div class="modal fade" id="EditModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel">Edit ?</h5>
+					<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">×</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<label for="basic-url">Judul</label>
+					<div class="input-group mb-3">
+						<input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+					</div>
+
+					<label for="basic-url">Upload Foto</label>
+					<div class="input-group mb-3">
+						<div class="custom-file">
+							<input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
+							<label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+						</div>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+					<a class="btn btn-primary" href="Index">Save</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Delete Modal-->
+	<div class="modal fade" id="DeleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel">Delete ?</h5>
+					<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">×</span>
+					</button>
+				</div>
+				<div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+				<div class="modal-footer">
+					<button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+					<a class="btn btn-primary" href="Index">Logout</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
 	<!-- Bootstrap core JavaScript-->
 	<script src="<?php echo base_url('assets/administrator'); ?>/assets/jquery/jquery.min.js"></script>
 	<script src="<?php echo base_url('assets/administrator'); ?>/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
